4ac6bb6438e1d9fb0dae5af4127f5bbdceac9918
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-11-23 07:26:15 +0000

    update

--- 1-vanilla/2-game-of-life.js
+++ 1-vanilla/2-game-of-life.js
@@ -8,20 +8,21 @@ class GameOfLife {
   tick() {
   }
 }
-describe('Game of Life', function () {
+
+describe('2 - Game of Life', function () {
   let gameOfLife;
   beforeEach(function () {
     gameOfLife = new GameOfLife();
   });
-  it('should make sure all cells are initially dead', function () {
+  it('1 - should make sure all cells are initially dead', function () {
     expect(gameOfLife.isCellAlive(2, 3)).toBe(false);
   });
-  it('should be able to toggle the state of individual cells', function () {
+  it('2 - should be able to toggle the state of individual cells', function () {
     gameOfLife.toggleCellState(2, 3);
 
     expect(gameOfLife.isCellAlive(2, 3)).toBe(true);
   });
-  it('should set the cell state to dead in next generation if the cell is alive in current generation and has less than 2 neighbours', function () {
+  it('3 - should set the cell state to dead in next generation if the cell is alive in current generation and has less than 2 neighbours', function () {
     gameOfLife.toggleCellState(2, 3);
 
     gameOfLife.tick();
