3694d458db289b25585850d9f8c320e3fe20548a
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-11-23 15:26:42 +0000

    update

---
+++ 1-vanilla/dont-peek/2-game-of-life.js
@@ -0,0 +1,41 @@
+class GameOfLife {
+  constructor() {
+    this.isAlive = {};
+  }
+  cellKey(row, column) {
+    return `${row}_${column}`;
+  }
+  isCellAlive(row, column) {
+    return this.isAlive[this.cellKey(row, column)] || false;
+  }
+  toggleCellState(row, column) {
+    var key = this.cellKey(row, column);
+    if (this.isAlive[key]) {
+      delete this.isAlive[key];
+    } else {
+      this.isAlive[key] = true;
+    }
+    return this;
+  }
+  tick() {
+    var key, parts, row, column, numberOfNeighbours = {}, neighbourKey;
+    for (key in this.isAlive) {
+      parts = key.split('_');
+      row = parseInt(parts[0], 10);
+      column = parseInt(parts[1], 10);
+      numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
+      [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function (offset) {
+        neighbourKey = this.cellKey(row + offset[0], column + offset[1]);
+        numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
+      }, this);
+    }
+    for (key in numberOfNeighbours) {
+      if (this.isAlive[key] && (numberOfNeighbours[key] < 2 || numberOfNeighbours[key] > 3) || !this.isAlive[key] && numberOfNeighbours[key] === 3) {
+        parts = key.split('_');
+        row = parseInt(parts[0], 10);
+        column = parseInt(parts[1], 10);
+        this.toggleCellState(row, column);
+      }
+    }
+  }
+}
---
+++ 1-vanilla/dont-peek/4-throttle.js
@@ -0,0 +1,22 @@
+const throttle = function (f, timeInMillis) {
+  var tsNextInvocationPossible = 0, nextArguments, timeout, apply;
+  apply = function (now, args) {
+    tsNextInvocationPossible = now + timeInMillis;
+    f.apply({}, args);
+  };
+  return function () {
+    var now = new Date().getTime();
+    if (now >= tsNextInvocationPossible) {
+      apply(now, arguments);
+    } else {
+      nextArguments = arguments;
+      if (!timeout) {
+        timeout = setTimeout(function () {
+          var now = new Date().getTime();
+          timeout = undefined;
+          apply(now, nextArguments);
+        }, tsNextInvocationPossible - now);
+      }
+    }
+  };
+};
